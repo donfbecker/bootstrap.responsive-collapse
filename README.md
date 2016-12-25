4ca7f4bade6fb4b85e9ef19ab6678abfbbd4244a
root <root@vm.psyon.org>
2016-12-25 15:37:00 -0600

    Initial import

---
+++ README.md
@@ -0,0 +1,36 @@
+A jQuery plugin for partially collapsing Bootstrap navbars.
+
+## Installation
+
+Include script *after* the jQuery library (unless you are packaging scripts somehow else):
+
+```html
+<script src="/path/to/jquery.responsive-collapse.js"></script>
+```
+
+**Do not include the script directly from GitHub (http://raw.github.com/...).** The file is being served as text/plain and as such being blocked
+in Internet Explorer on Windows 7 for instance (because of the wrong MIME type). Bottom line: GitHub is not a CDN.
+
+## Usage
+
+Create session cookie:
+
+```javascript
+$.responsiveCollapse('.navbar-nav');
+```
+
+## Customization
+
+```javascript
+$.responsiveCollapse('.navbar-nav', {
+	// Bootstraps xs breakpoint
+	breakPoint: 768,
+
+	// Content for overflow menu item
+	overflowItemText: '<i class="fa fa-bars"></i> More <b class="caret"></b>'
+});
+```
+
+## Authors
+
+[Donald Becker](https://github.com/psyon)
---
+++ src/jquery.responsive-collapse.js
@@ -0,0 +1,93 @@
+(function ($) {
+	$.fn.responsiveCollapse = function(options) {
+		var settings = $.extend({
+			breakPoint:       768,
+			overflowItemText: '<i class="fa fa-bars"></i> More <b class="caret"></b>'
+		}, options);
+
+		var lastDocWidth = $(document).width();
+
+		return this.filter('ul').each(function() {
+			var $list        = $(this);
+			var container    = $list.parent().parent();
+			var overflowItem = $('<li class="dropdown hide"><a href="#" class="dropdown-toggle" data-toggle="dropdown">' + settings.overflowItemText + '</a><ul class="dropdown-menu dropdown-menu-right"></ul></li>').appendTo($list);
+			var overflowList = $(overflowItem).find('ul.dropdown-menu');
+
+			var thisWidth    = $list.outerWidth();
+			var maxWidth     = $(container).width() - $(container).find('.navbar-header').width();
+
+			function reset() {
+				var child = $(overflowList).children();
+				var count = child.size();
+				if (count > 0) {
+					for (var i = 0; i < count; i++) {
+						$(child[i]).insertBefore($list.children(':last-child'));
+					}
+				}
+				$(overflowItem).addClass('hide');
+			}
+
+			function grow() {
+				var overflowWidth = $(overflowItem).width();
+				var child         = $(overflowList).children();
+				var count         = child.size();
+
+				if (count > 0) {
+					for (var i = 0; i < count; i++) {
+						// move the first dropdown item to end of list
+						$(child[i]).insertBefore($list.children(':last-child'));
+						thisWidth = $list.outerWidth();
+						if((i == count - 1 && thisWidth - overflowWidth > maxWidth) || (i != count - 1 && thisWidth > maxWidth)) {
+							$(child[i]).prependTo(overflowList);
+							break;
+						}
+					}
+				}
+
+				// Hide the overflow item if it has no children
+				if($(overflowList).children().size() == 0) {
+					$(overflowItem).addClass('hide');
+				}
+
+				return;
+			}
+
+			function shrink() {
+				var child = $list.children(':not(:last-child)');
+				var count = child.size();
+				if (count < 1) return;
+
+				// show the overflow link in case it's been hidden
+				$(overflowItem).removeClass('hide');
+
+				for (var i = count - 1; i >= 0; i--) {
+					// move the last item to dropdown
+					$(child[i]).prependTo(overflowList);
+
+					// recalc width
+					thisWidth = $list.outerWidth();
+					if(thisWidth < maxWidth) break;
+				}
+			}
+
+			function refresh() {
+				thisWidth = $list.outerWidth();
+				maxWidth  = $(container).width() - $(container).find('.navbar-header').width();
+				var docWidth  = $(document).width();
+
+				if ($(window).width() < settings.breakPoint) {
+					reset();
+				} else if (thisWidth > maxWidth) {
+					shrink();
+				} else if (0 < (docWidth - lastDocWidth)) {
+					grow();
+				}
+				lastDocWidth = docWidth;
+			}
+
+			$(window).resize(function() {
+				refresh();
+			}).trigger('resize');
+		});
+	}
+}(jQuery));
